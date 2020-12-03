bfdd32c36ac5caf5ce36e97532be304a6fa70145
rahul975337 <tilwani03@gmail.com>
2020-12-04 02:53:43 +0530

    footer done

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,12 @@
 import React from "react";
 import "./Footer.css";
 function Footer() {
-  return <div></div>;
+  return (
+    <div className="footer">
+      <p>© 2020 Rahul Tilwani! </p>
+      <p>Privacy · Terms · Sitemap · Company Details</p>
+    </div>
+  );
 }
 
 export default Footer;
